b8a5ab67f3d09b3bc6d6125056cbe189fc52ba07
longzekai@sina.com <mz2034146>
2014-06-14 22:29:45 +0800

    接受和解析服务器返回的注册信息

--- LifeStyle/src/cn/com/danny/lifestyle/net/ParseJsonDataService.java
+++ LifeStyle/src/cn/com/danny/lifestyle/net/ParseJsonDataService.java
@@ -1,12 +1,17 @@
 package cn.com.danny.lifestyle.net;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import cn.com.danny.lifestyle.MainActivity;
+import android.R.integer;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.util.Log;
 
 public class ParseJsonDataService extends Service {
 
@@ -29,7 +34,12 @@ public class ParseJsonDataService extends Service {
 					URLParam param = new URLParam(null) ;
 					param.addParam("name", name) ;
 					param.addParam("password", psw) ;
-					docmd(URLProtocol.CMD_REGISTER, param) ;
+					Bundle result = docmd(URLProtocol.CMD_REGISTER, param) ;
+					Message message = new Message() ;
+					message.what = URLProtocol.CMD_REGISTER ;
+					message.setData(result) ;
+					//最后用handler 发送出去。
+					
 					break;
 
 				default:
@@ -51,6 +61,36 @@ public class ParseJsonDataService extends Service {
 		URLParam _param = new URLParam(param) ;
 		_param.addParam("cmd", cmd) ;
 		String jsonStr = HttpConnection.HttpGet(URLProtocol.ROOT, _param) ;
+		Log.i("lifestyle", jsonStr) ;
+		//把Json中的特殊符号过滤掉
+		jsonStr.replaceAll("(\r\n|\r|\n|\n\r)", " ") ;
+		
+		try {
+			JSONObject json = new JSONObject(jsonStr) ;
+			int code = json.getInt("code") ; //return code tag ;
+			result.putInt("code", code) ;
+			
+			int returnCmd = json.getInt("cmd") ; // request code tag ;
+			if (returnCmd != cmd) {
+				Log.e("lifestyle", "Data Error!") ;
+			}
+			result.putInt("cmd", cmd) ;
+			//向服务器发送请求成功
+			if (code == 0) {
+				switch (cmd) {
+				case URLProtocol.CMD_REGISTER :
+					// 获取登录的uid
+					String uid = json.getString("uid") ;
+					break;
+				}
+			} else {
+				Log.e("lifestyle", "Server Error! ") ;
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return result ;
 		
 //		URLParam _param = new URLParam(param) ;
